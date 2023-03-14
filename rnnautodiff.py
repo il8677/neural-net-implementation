Fixed derivative bug

rnnautodiff.py
@@ -105,7 +105,7 @@ class FeedforwardLayer(Layer):
 
     def propogate(self, X):
         self.input = X
-        self.H = np.dot(X, self.W)
+        self.H = np.dot(self.W, X)
         return super().propogate(X)
     
     def backpropogate(self, acc=1):
@@ -121,18 +121,11 @@ class FeedforwardLayer(Layer):
         return dldh * self.dhdi()
     
     def dhdi(self):
-        return self.W
+        return np.dot(self.W, np.ones_like(self.input))
 
     def dhdw(self):
-        M = np.zeros_like(self.W)
-
-        for i in range(M.shape[0]):
-            for j in range(M.shape[1]):
-                M[i][j] = self.input[i] if not np.isscalar(self.input) else self.input
-
-        return M
         #return self.input * np.ones_like(self.W)
-        #return np.dot(self.input, np.ones_like(self.W))
+        return np.dot(self.input, np.ones_like(self.W))
 
     def print(self):
         print(Layer.getHeader())

tests.py
@@ -1,10 +1,7 @@
 from rnnautodiff import *
 import numpy as np
 import unittest
-
-def testModelDerivatives():
-    epsilon = 1e-15
-
+'''
 class StaticTests(unittest.TestCase):
     def test_ff_deriv11(self):
         ffl = FeedforwardLayer(1, 1)
@@ -49,10 +46,57 @@ class StaticTests(unittest.TestCase):
             [20,20,20]
         ])
         np.testing.assert_array_equal(ffl.dhdw(), correctAns)
+'''
         
 
-class NumericalChecks(unittest):
-    pass
+class NumericalChecks(unittest.TestCase):
+    epsilon = 1e-7
+    acceptableMargin = 1e-7
+
+    def test_ff_dhdw(self):
+        ffl = FeedforwardLayer(3,3)
+
+        # test dhdw
+        X = np.array([2,3,4], np.float64)
+        ffl.W = np.array([
+            [-2, -3, -4],
+            [2, 3, 4],
+            [-2, -3, -4]
+        ], np.float64)
+        f1 = ffl.propogate(X)
+        ffl.W += self.epsilon
+        f2 = ffl.propogate(X)
+        ffl.W -= self.epsilon
+
+        ndf = (f2 - f1)/(self.epsilon)
+        sdf = ffl.dhdw()
+
+        diff = np.abs(sdf - ndf)
+
+        self.assertLess(np.mean(diff), self.acceptableMargin)
+
+
+    def test_ff_dhdi(self):
+        ffl = FeedforwardLayer(3,3)
+
+        # test dhdw
+        X = np.array([2,3,4], np.float64)
+        ffl.W = np.array([
+            [-2, -3, -4],
+            [2, 3, 4],
+            [5, 6, 7]
+        ], np.float64)
+        f1 = ffl.propogate(X)
+        sdf = ffl.dhdi()
+        X += self.epsilon
+        f2 = ffl.propogate(X)
+        X -= self.epsilon
+
+        ndf = (f2 - f1)/(self.epsilon)
+
+        diff = np.abs(sdf - ndf)
+        self.assertLess(np.mean(diff), self.acceptableMargin)
+
 
 if __name__=="__main__":
     unittest.main()