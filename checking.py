2024eec30b6db2ef52f4ac9bb699a5d6462edde7
il8677 <jiajiaisaac@gmail.com>
2023-03-30 07:40:56 +0200

    Restructured folders

--- checking.py
+++
@@ -1,8 +0,0 @@
-
-# Checks the gradients by manually testing the rate of change
-def check_derivative(x, y, dy):
-    epsilon = 1e-4
-    ngrad = y(x+epsilon) - y(x-epsilon)
-    ngrad /= 2*epsilon
-
-    return ngrad / dy(x)
